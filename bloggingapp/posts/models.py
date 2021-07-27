0a506b3de420d988e83d9caac41f7e23e850c29d
ankit <ankitkmr039@gmail.com>
2021-07-27 12:10:58 +0530

    final commit

--- bloggingapp/posts/models.py
+++ bloggingapp/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 
-class posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=10000)
-    created_at = models.DateTimeField(default=datetime.now , blank= True)
+    created_at = models.DateTimeField(default = datetime.now , blank= True)
