43230c03f918ec9a0e7bae9fa02781afce4cf4de
ankit <ankitkmr039@gmail.com>
2021-12-21 16:14:19 +0530

    changes in templates

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,16 @@ from .models import Post
 # Create your views here.
 from django.views.generic import ListView
 from django.views import View
+from django.views.generic import TemplateView
 
-class BlogIndexView(ListView):
-    model=Post
+
+class IndexView(TemplateView):
     template_name="posts/index.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
 
     
 def post(request ,pk):
