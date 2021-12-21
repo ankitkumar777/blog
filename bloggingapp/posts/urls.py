6f15d66deb37844f1e2217b7324697c8a59d908a
ankit <ankitkmr039@gmail.com>
2021-12-21 12:22:17 +0530

    resuming the project

--- bloggingapp/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.BlogIndexView.as_view(), name = 'index'),
-    path('post/<str:pk>', views.post, name = 'post')
-]
